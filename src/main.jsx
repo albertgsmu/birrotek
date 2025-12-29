06136203635b64a421ebadb9537057c664ba89b6
albertgsmu <albertgsmu@gmail.com>
2025-12-29 18:01:21 -0500

    Fix base path for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-if ('serviceWorker' in navigator && import.meta.env.PROD) {
-  if (location.hostname !== 'albertgsmu.github.io') {
-    navigator.serviceWorker.register('/service-worker.js')
-  }
-}
