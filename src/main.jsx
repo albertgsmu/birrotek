c3e29060e92415da4410f6898f5c4ca347c697ed
albertgsmu <albertgsmu@gmail.com>
2025-12-29 14:29:34 -0500

    disable PWA for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 );
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
+    //navigator.serviceWorker.register("/service-worker.js");
   });
 }
